40afb858c5fc8a3d76f6ef836e3f64b3d91f13ed
Komron Bakozoda <kbakozoda@twilio.com>
2021-03-20 22:18:57 +0100

    Add entityRecognition service. Change the old controller for text parsing

--- space-jungle-api/src/entity-recognition/entityRecognition.service.ts
+++ space-jungle-api/src/entity-recognition/entityRecognition.service.ts
@@ -1,13 +1,81 @@
 import axios from 'axios';
 
-class EntityRecognitionService {
-    constructor() {
-        console.log("AZ_ENDPOINT = ", process.env.AZ_ENDPOINT);
-    }
+const EntityRecognitionService = {
+    getTextAsChunks: (text: string): String[] => {
+        // This method is buggy, it might split in the middle of a word
+        // but for sake of saving time this bug is ignored.
+        const maxLength = process.env.AZ_API_TEXT_MAX_LEN;
+        
+        return text.match(/(.|[\r\n]){1,1000}/g);
+    },
 
-    public async getEntitiesForText(text: string) {
-        let res = await axios.post(process.env.AZ_ENDPOINT, { id: 1, language: "en", text });
-    }
+    splitTextIntoTrimmedSentences: (text: string): String[] => {
+        // TODO: optimize this lol
+        const sentences = text.match( /[^\.!\?]+[\.!\?]+/g ).map(
+            s => s
+                .replace(`”`, "")
+                .replace(`“`, "")
+                .replace('(',"")
+                .replace(`)`,"")
+                .trim()
+                .replace(/(\r\n|\n|\r)/gm, "")
+                .replace("\t", "")
+        );
+
+        return sentences;
+    },
+    
+    getEntitiesInfoForChunks: async (chunks: String[]) => {
+        console.log("AZ_ENDPOINT = ", process.env.AZ_ENDPOINT);
+
+        const data = { documents: [] };
+
+        const docs = chunks.map((str, i) => {
+            return { id: i+1, language: "en", text: str };
+        });
+
+        data.documents = docs;
+
+        console.log("AZ body=", data);
+
+        let res = await axios.post(process.env.AZ_ENDPOINT, data );
+        console.log(res.data.documents);
+        return res.data;
+    },
+
+    getEntitySentencesForText: async (text: string) => {
+        const textChunks = EntityRecognitionService.getTextAsChunks(text);
+        const sentences = EntityRecognitionService.splitTextIntoTrimmedSentences(text);
+        const entitiesInfo = await EntityRecognitionService.getEntitiesInfoForChunks(textChunks);
+
+        // Go over each sentence and mark whether we take it
+        const entitySentencesIndexToFlagMap = {};
+        const markedSentences = [];
+        sentences.forEach((s, i) => entitySentencesIndexToFlagMap[i] = false);
+
+        entitiesInfo.documents.forEach(
+            doc => {
+                doc.entities.forEach(
+                    entity => {
+                        const entityText = entity.text;
+                        sentences.forEach((s, i) => {
+                            if (sentences[i].includes(entityText)) {
+
+                                if (!entitySentencesIndexToFlagMap[i]) {
+                                    markedSentences.push(s);
+                                    console.log(`${s} INCLUDES ${entityText}`);
+                                }
+
+                                entitySentencesIndexToFlagMap[i] = true;
+                            }
+                        });
+                    }
+                )
+            }
+        );
+
+        return markedSentences;
+    },
 }
 
 export default EntityRecognitionService;
--- space-jungle-api/src/server.ts
+++ space-jungle-api/src/server.ts
@@ -1,13 +1,12 @@
 import * as dotenv from 'dotenv';
 import App from './app';
-import UserController from './user/user.controller';
-import TextParserController from './textParser/textParser.controller';
+import TrustScoreController from './trustScore/trustScore.controller';
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 // Express server
-const app = new App([new UserController(), new TextParserController()]);
+const app = new App([new TrustScoreController()]);
 
 app.listen();
