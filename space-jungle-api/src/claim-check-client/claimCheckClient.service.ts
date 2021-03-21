46e841988e2ed0b34ec8492239d4dbe30f55814e
Komron Bakozoda <kbakozoda@twilio.com>
2021-03-21 08:40:59 +0100

    Minor fixes

--- space-jungle-api/src/claim-check-client/claimCheckClient.service.ts
+++ space-jungle-api/src/claim-check-client/claimCheckClient.service.ts
@@ -1,5 +1,12 @@
+import axios from 'axios';
+
 const ClaimCheckClientService = {
-    
+    checkClaims: async (claims: String[]) => {
+        console.log("CLAIM_CHECK_API = ", process.env.CLAIM_CHECK_API);
+        let claimsInfo = await axios.post(process.env.CLAIM_CHECK_API, claims);
+
+        return claimsInfo;
+    }
 };
 
 export default ClaimCheckClientService;
